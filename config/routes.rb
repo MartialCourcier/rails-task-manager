2bdd051ccdb3eec02f5277c437c7fb6396debfc4
Martial Courcier <martialcourcier@sfr.fr>
2023-02-21 17:15:44 +0100

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get "tasks/:id", to: "tasks#show", as: :task
 
   post "tasks", to: "tasks#create"
-  get "tasks/:id/edit", to: "tasks#edit"
+  get "tasks/:id/edit", to: "tasks#edit", as: :modify_task
   patch "tasks/:id", to: "tasks#update"
   delete "tasks/:id", to: "tasks#destroy"
 
